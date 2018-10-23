28d182586be2feb5ff20a618822cf9670bedc6fa
Aleksey Poddubnuy <aprduction@gmail.com>
2018-10-23 17:59:39 +0300

    Create categories with slug

---
+++ db/migrate/20181023145703_create_categories.rb
@@ -0,0 +1,13 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :title
+      t.string :body
+      t.text :desc
+      t.string :slug
+
+      t.timestamps
+    end
+    add_index :categories, :slug, unique: true
+  end
+end
